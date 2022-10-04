851fc8cb94068bce69200439d1310840f2e81aa2
Alex <kalfas.alex@gmail.com>
2022-10-04 20:41:28 +0300

    Add command that deletes channels in category

--- FetaWarrior/DiscordFunctionality/ChannelCategoryModule.cs
+++ FetaWarrior/DiscordFunctionality/ChannelCategoryModule.cs
@@ -2,11 +2,17 @@
 using Discord.Interactions;
 using Discord.WebSocket;
 using FetaWarrior.Extensions;
+using Garyon.Extensions;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace FetaWarrior.DiscordFunctionality;
 
+[Group("category", "Adjust multiple channels inside categories")]
 [EnabledInDm(false)]
 [RequireContext(ContextType.Guild)]
 [RequireUserPermission(GuildPermission.ManageChannels)]
@@ -14,7 +20,7 @@ namespace FetaWarrior.DiscordFunctionality;
 public class ChannelCategoryModule : SocketInteractionModule
 {
     [EnabledInDm(false)]
-    [SlashCommand("category", "Adjust channels' parent categories")]
+    [SlashCommand("move", "Adjust channels' parent categories")]
     public async Task MoveChannels
     (
         [Summary(description: "The channel category whose channels to move (leave empty for uncategorized channels)")]
@@ -61,13 +67,213 @@ public class ChannelCategoryModule : SocketInteractionModule
             await channel.MoveToCategoryAsync(targetCategory);
         }
 
-        if (deleteOriginalCategory && originalCategorySocket is not null)
+        if (deleteOriginalCategory && originalCategory is not null)
         {
             await UpdateResponseTextAsync("Deleting the category channel...");
 
-            await originalCategorySocket.DeleteAsync();
+            await originalCategory.DeleteAsync();
         }
 
-        await UpdateResponseTextAsync("Successfully moved the channels");
+        await UpdateResponseTextAsync("Successfully moved the channels.");
+    }
+
+    [EnabledInDm(false)]
+    [SlashCommand("delete", "Delete all channels belonging to a category")]
+    public async Task DeleteChannels
+    (
+        [Summary(description: "The channel category whose channels to delete (leave empty for uncategorized channels)")]
+        ICategoryChannel parentCategory = null,
+
+        [Summary(description: "Set this to true to delete the original category (if any) after deleting all the channels")]
+        bool deleteCategory = false
+    )
+    {
+        var filters = await ShowChannelFilterMenu();
+
+        var targetChannels = Context.Guild.ChannelsInCategory(parentCategory?.Id);
+        var filteredChannels = Filter(targetChannels, filters).ToArray();
+
+        foreach (var channel in filteredChannels)
+        {
+            await channel.DeleteAsync();
+        }
+
+        if (deleteCategory && parentCategory is not null)
+        {
+            await UpdateResponseTextAsync("Deleting the category channel...");
+
+            await parentCategory.DeleteAsync();
+        }
+
+        await UpdateResponseTextAsync("Successfully deleted the channels of the specified types.");
+    }
+
+    private static IEnumerable<IGuildChannel> Filter(IEnumerable<IGuildChannel> channels, ChannelTypeFilterArguments arguments)
+    {
+        return channels.Where(c => arguments.PassesFilter(c));
+    }
+
+    private static readonly NestedChannelType[] nestedChannelTypes =
+    {
+        NestedChannelType.Text,
+        NestedChannelType.Voice,
+        NestedChannelType.Stage,
+        NestedChannelType.Forum,
+        NestedChannelType.Announcement,
+    };
+
+    protected async Task<ChannelTypeFilterArguments> ShowChannelFilterMenu()
+    {
+        var deferral = DeferAsync();
+
+        var menuBuilder = new SelectMenuBuilder()
+            .WithPlaceholder("Select the channel types")
+            .WithCustomId("channel_type_menu")
+            .WithMinValues(1)
+            .WithMaxValues(nestedChannelTypes.Length);
+
+        foreach (var channelType in nestedChannelTypes)
+        {
+            var channelTypeName = channelType.ToString();
+            menuBuilder.AddOption(channelTypeName, channelTypeName);
+        }
+
+        var componentBuilder = new ComponentBuilder()
+            .WithSelectMenu(menuBuilder);
+
+        const string forumChannelWarning = "**NOTE: Forum channels cannot be discovered in channel categories for the time being. The option will be ignored.**";
+
+        await deferral;
+        var selectionMessage = await FollowupAsync(text: $"Select the channel types to delete:\n{forumChannelWarning}", components: componentBuilder.Build(), ephemeral: true);
+
+        var menuResponse = await TrackSelectMenuResponseAsync(selectionMessage.Id, Context.Interaction.User, null);
+        var values = menuResponse.Data.Values;
+
+        var delimitedValues = string.Join(", ", values);
+        await selectionMessage.ModifyAsync(m =>
+        {
+            m.Content = $"Deleting channel types: **{delimitedValues}**";
+            m.Components = new Optional<MessageComponent>(null);
+        });
+
+        var types = TypesForStringValues(values);
+        return ChannelTypeFilterArguments.FromTypes(types);
+    }
+
+    private IEnumerable<NestedChannelType> TypesForStringValues(IEnumerable<string> values)
+    {
+        return values.Select(TypesForStringValue);
+    }
+    private NestedChannelType TypesForStringValue(string value)
+    {
+        return value switch
+        {
+            nameof(NestedChannelType.Text) => NestedChannelType.Text,
+            nameof(NestedChannelType.Voice) => NestedChannelType.Voice,
+            nameof(NestedChannelType.Stage) => NestedChannelType.Stage,
+            nameof(NestedChannelType.Announcement) => NestedChannelType.Announcement,
+            nameof(NestedChannelType.Forum) => NestedChannelType.Forum,
+        };
+    }
+
+    private async Task<SocketMessageComponent> TrackSelectMenuResponseAsync(ulong messageID, IUser guildUser, Func<SocketMessageComponent, Task> modalSubmitted)
+    {
+        BotClientManager.Instance.Client.SelectMenuExecuted += FireOnSpecificToken;
+
+        SocketMessageComponent targetMenu = null;
+        while (targetMenu is null)
+        {
+            await Task.Delay(50);
+        }
+
+        BotClientManager.Instance.Client.SelectMenuExecuted -= FireOnSpecificToken;
+        return targetMenu;
+
+        async Task FireOnSpecificToken(SocketMessageComponent messageComponent)
+        {
+            if (messageComponent.Message.Id != messageID)
+                return;
+
+            if (messageComponent.User.Id != guildUser.Id)
+                return;
+
+            // Allow removing the event as quickly as possible
+            targetMenu = messageComponent;
+
+            if (modalSubmitted is not null)
+            {
+                await modalSubmitted(messageComponent);
+            }
+        }
+    }
+
+    private async Task<SocketModal> TrackModalResponseAsync(string interactionToken, Func<SocketModal, Task>? modalSubmitted)
+    {
+        BotClientManager.Instance.Client.ModalSubmitted += FireOnSpecificToken;
+
+        SocketModal targetModal = null;
+        while (targetModal is null)
+        {
+            await Task.Delay(50);
+        }
+
+        BotClientManager.Instance.Client.ModalSubmitted -= FireOnSpecificToken;
+        return targetModal;
+
+        async Task FireOnSpecificToken(SocketModal modal)
+        {
+            if (modal.Token != interactionToken)
+                return;
+
+            // Allow removing the event as quickly as possible
+            targetModal = modal;
+
+            if (modalSubmitted is not null)
+            {
+                await modalSubmitted(modal);
+            }
+        }
+    }
+
+    protected async Task<ChannelTypeFilterArguments> AskChannelTypeFilterArguments()
+    {
+        var filters = new ChannelTypeFilterArguments();
+
+        var componentBuilder = new ComponentBuilder()
+
+            .WithButton(label: nameof(ChannelTypeFilterArguments.Text),
+                        customId: nameof(ChannelTypeFilterArguments.Text),
+                        style: ToggledButtonStyle(filters.Text))
+
+            .WithButton(label: nameof(ChannelTypeFilterArguments.Voice),
+                        customId: nameof(ChannelTypeFilterArguments.Voice),
+                        style: ToggledButtonStyle(filters.Voice))
+
+            .WithButton(label: nameof(ChannelTypeFilterArguments.Stage),
+                        customId: nameof(ChannelTypeFilterArguments.Stage),
+                        style: ToggledButtonStyle(filters.Stage))
+
+            .WithButton(label: nameof(ChannelTypeFilterArguments.Announcement),
+                        customId: nameof(ChannelTypeFilterArguments.Announcement),
+                        style: ToggledButtonStyle(filters.Announcement))
+
+            .WithButton(label: nameof(ChannelTypeFilterArguments.Forum),
+                        customId: nameof(ChannelTypeFilterArguments.Forum),
+                        style: ToggledButtonStyle(filters.Forum));
+
+
+        await RespondAsync(
+            ephemeral: true,
+            components: componentBuilder.Build());
+
+        return filters;
+    }
+    private static ButtonStyle ToggledButtonStyle(bool toggled)
+    {
+        return toggled switch
+        {
+            true => ButtonStyle.Success,
+            false => ButtonStyle.Danger,
+        };
     }
 }
---
+++ FetaWarrior/DiscordFunctionality/ChannelTypeFilterArguments.cs
@@ -0,0 +1,60 @@
+﻿using Discord;
+using System.Collections.Generic;
+
+namespace FetaWarrior.DiscordFunctionality;
+
+public record struct ChannelTypeFilterArguments(bool Text, bool Voice, bool Announcement, bool Stage, bool Forum)
+{
+    public bool PassesFilter(IGuildChannel channel)
+    {
+        return channel.GetChannelType() switch
+        {
+            ChannelType.Text => Text,
+            ChannelType.Voice => Voice,
+            ChannelType.News => Announcement,
+            ChannelType.Stage => Stage,
+            ChannelType.Forum => Forum,
+
+            _ => false,
+        };
+    }
+
+    public void SetForChannelType(NestedChannelType nestedChannelType, bool enabled)
+    {
+        // Ugly as fuck because no switch on refs + no property refs
+        switch (nestedChannelType)
+        {
+            case NestedChannelType.Text: 
+                Text = enabled;
+                break;
+
+            case NestedChannelType.Voice:
+                Voice = enabled;
+                break;
+
+            case NestedChannelType.Announcement:
+                Announcement = enabled;
+                break;
+
+            case NestedChannelType.Stage:
+                Stage = enabled;
+                break;
+
+            case NestedChannelType.Forum:
+                Forum = enabled;
+                break;
+        };
+    }
+
+    public static ChannelTypeFilterArguments FromTypes(IEnumerable<NestedChannelType> channelTypes)
+    {
+        var result = new ChannelTypeFilterArguments();
+
+        foreach (var channelType in channelTypes)
+        {
+            result.SetForChannelType(channelType, true);
+        }
+
+        return result;
+    }
+}
